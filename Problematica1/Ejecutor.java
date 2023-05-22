5ef5008bdbf436a0fb24bb0b4d626592bff412be
Joseph  Andres Ramirez Correa <jaramirez37@utpl.edu.ec>
2023-05-21 23:01:05 -0500

    taller6

---
+++ Problematica1/Ejecutor.java
@@ -0,0 +1,7 @@
+public class Ejecutor {
+
+    Medico medico = new Medico("Thomas", "Cirujano", 2345);
+    Ciudad ciudad = new Ciudad("Gh", "Arkh");
+    Enfermero efermero = new Enfermero("James", "Contrato", 234);
+    Hospital hospital = new Hospital("Arkh Hospital", "Gh", 7, medico, enfermero, "Arkham");
+}
